f627635f792f4259557b659e6ea9bd7608d0de58
kuanmol <ramuklomna100@gmail.com>
2025-01-30 10:42:50 +0530

    dogcathuman

--- gpu-metrics.py
+++ gpu-metrics.py
@@ -1,22 +1,3 @@
-from keras.api.models import load_model
-from keras_preprocessing import image
-import numpy as np
-
-from keras_preprocessing.image import ImageDataGenerator
-
-loaded_model = load_model('my_image_classifier_model.keras')
-
-
-test_image = image.load_img('img_11.png', target_size=(128, 128))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-test_image = test_image / 255.0
-
-result = loaded_model.predict(test_image)
-class_index = np.argmax(result[0])
-class_labels = {0: 'cat', 1: 'dog', 2: 'human'}
-prediction = class_labels[class_index]
-print(f"Prediction: {prediction}")
 
 from keras.api.models import load_model
 from keras_preprocessing.image import ImageDataGenerator
@@ -57,4 +38,3 @@ class_labels = list(test_generator.class_indices.keys())
 
 # Print classification report
 print(classification_report(y_true, y_pred, target_names=class_labels))
-
